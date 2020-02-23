77ffaab698f6e8abbf59e2aa5f97c12c161163f7
miaogen <miaogen156@gmail.com>
2020-02-23 15:10:37 +0800

    add CH node server

--- ConsistentHashNode/CH_node.cpp
+++ ConsistentHashNode/CH_node.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <thread>
 
 #include <grpcpp/grpcpp.h>
 
@@ -29,9 +30,31 @@ int main(int argc, char** argv) {
   int ret = get_host_info(host_name, ip);
   if(ret){
     ERROR("get hostip error");
+    exit(-1);
   }
+  INFO(ip);
   conf.set("host_ip", ip);
+  std::string port = conf.get("CH_node_port");
+  if(port.size()==0){
+    ERROR("get port error");
+    exit(-1);
+  }
+  std::string server_address("0.0.0.0:"+port);
   CH_node_impl node{conf};
+
+  grpc::ServerBuilder builder;
+  // Listen on the given address without any authentication mechanism.
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  // Register "service" as the instance through which we'll communicate with
+  // clients. In this case it corresponds to an *synchronous* service.
+  builder.RegisterService(&node);
+  // Finally assemble the server.
+  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+  std::cout << "CH node listening on " << server_address << std::endl;
+
+//  std::thread server_ack(grpc::Server::Wait, &server);
+//  server_ack.detach();
   node.run();
+  server->Wait();
   return 0;
 }
