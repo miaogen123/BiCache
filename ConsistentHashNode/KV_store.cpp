3abb79153901c7f34c5ffb8b153fc5ae435e076b
miaogen <miaogen156@outlook.com>
2020-03-30 09:55:44 +0800

    support key expire

--- ConsistentHashNode/KV_store.cpp
+++ ConsistentHashNode/KV_store.cpp
@@ -17,7 +17,7 @@ uint64_t get_miliseconds(){
 KV_store_impl::KV_store_impl(Conf& conf):inner_conf_(conf){
     auto bucket_size = conf.get("bucket_size", "10000");
     inner_cache_.reserve(atoi(bucket_size.c_str()));
-    inner_cache_["hello"]="world";
+    inner_cache_["hello"]=std::make_pair<uint64_t, std::string>(get_miliseconds(), "world");
 }
 
 int KV_store_impl::is_valid(uint64_t& timestamp, int& req_id, uint64_t& key_pos, std::string& msg){
@@ -45,7 +45,7 @@ int KV_store_impl::is_valid(uint64_t& timestamp, int& req_id, uint64_t& key_pos,
     msg = "this key is not in my range";
     return -3;
   }
-
+  return 0;
 }
 
 //TODO:: 有些接口定义的不是很同一（同时存在 Response&Reply）
@@ -63,18 +63,22 @@ int KV_store_impl::is_valid(uint64_t& timestamp, int& req_id, uint64_t& key_pos,
   if(ret == -3){
     int close_one = -1;
     ch_node_->find_closest_preceding_finger(key_pos, close_one, ch_node_->get_finger_table());
+    info("close one {} to key_pos {}", close_one, key_pos);
     rsp->set_close_pos(close_one);
   }
   if(ret<0){
     rsp->set_status_code(ret);
     return {grpc::StatusCode::OK, ""};
   }
-  auto ite = inner_cache_.find(key);
-  if(ite==inner_cache_.end()){
-    rsp->set_value("");
-  }else{
-    rsp->set_is_found(true);
-    rsp->set_value(ite->second);
+  {
+    std::shared_lock<std::shared_mutex> r_lock_for_cache(rw_lock_for_cache_);
+    auto ite = inner_cache_.find(key);
+    if(ite==inner_cache_.end()){
+      rsp->set_value("");
+    }else{
+      rsp->set_is_found(true);
+      rsp->set_value(ite->second.second);
+    }
   }
   //流量小的时候可以，大的时候就不能打这种log了
   //info("Get from ")
@@ -100,11 +104,23 @@ int KV_store_impl::is_valid(uint64_t& timestamp, int& req_id, uint64_t& key_pos,
     rsp->set_status_code(ret);
     return {grpc::StatusCode::OK, ""};
   }
-  auto ite = inner_cache_.find(key);
-  if(ite!=inner_cache_.end()){
+  {
+    std::unique_lock<std::shared_mutex> w_lock_(rw_lock_for_cache_);
+    auto ite = inner_cache_.find(key);
     rsp->set_is_set(true);
-    ite->second = req->value();
-    info("set key {} value {}", key, req->value());
+    std::string value = req->value();
+    uint64_t timestamp = get_miliseconds();
+    if(ite==inner_cache_.end()){
+      ite = inner_cache_.insert({key, std::make_pair<uint64_t, std::string>(std::move(timestamp), std::move(value))}).first;
+    }else{
+      ite->second.second = value;
+    }
+    ite->second.first = (timestamp + req->update_times());
+    //info("set key {} value {} {}", key, req->value(), ite->second.first);
+    if(req->update_times()>0){
+      expire_queue_.push( std::make_pair(ite->second.first, ite) );
+      info("update expire time {} to {}: update_times {}",  key, timestamp, req->update_times());
+    }
   }
   //流量小的时候可以，大的时候就不能打这种log了
   //info("Get from ")
@@ -112,57 +128,86 @@ int KV_store_impl::is_valid(uint64_t& timestamp, int& req_id, uint64_t& key_pos,
 }
 
 void KV_store_impl::init(){
-CH_node_serving_thr_ = std::make_shared<std::thread>(&KV_store_impl::run_CH_node, this);
-backend_update_thr_ = std::make_shared<std::thread>(&KV_store_impl::backend_update, this);
+  CH_node_serving_thr_ = std::make_shared<std::thread>(&KV_store_impl::run_CH_node, this);
+  backend_update_thr_ = std::make_shared<std::thread>(&KV_store_impl::backend_update, this);
 }
 
 //单独的线程用来服务
 void KV_store_impl::run_CH_node(){
-std::string CH_node_port = inner_conf_.get("CH_node_port");
-if(CH_node_port.size()==0){
-  spdlog::critical("get CH_node_port error");
-  exit(-1);
-}
+  std::string CH_node_port = inner_conf_.get("CH_node_port");
+  if(CH_node_port.size()==0){
+    spdlog::critical("get CH_node_port error");
+    exit(-1);
+  }
 
-std::string server_address("0.0.0.0:"+CH_node_port);
-ch_node_ = std::make_shared<CH_node_impl>(inner_conf_);
-//CH_node_impl node{inner_conf};
-grpc::ServerBuilder builder;
-builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-builder.RegisterService(ch_node_.get());
-std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
-spdlog::info("CH node listening on {}", server_address);
-ch_node_->run();
-server->Wait();
+  std::string server_address("0.0.0.0:"+CH_node_port);
+  ch_node_ = std::make_shared<CH_node_impl>(inner_conf_);
+  //CH_node_impl node{inner_conf};
+  grpc::ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(ch_node_.get());
+  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+  spdlog::info("CH node listening on {}", server_address);
+  ch_node_->run();
+  server->Wait();
 }
 
 void KV_store_impl::backend_update(){
-info("start cleaning: cleaning interval = 2s, info log interval = 10s");
-//TODO::这里先清理reqids，后面添加了 key 超时以后再设置 key 清理的逻辑
-int count = 0;
-int expire_clean_count = 0;
-do{
-  count++;
-  usleep(clean_interval_);
-  {
-    std::unique_lock<std::shared_mutex> w_lock_for_ids(rw_lock_for_ids_);
-      auto cur_miliseconds = get_miliseconds();
-      //2s内的请求应该不多，直接遍历就好
-      for(auto ite = req_ids_.begin();ite!= req_ids_.end();){
-        if(ite->second < cur_miliseconds){
-          ite = req_ids_.erase(ite);
-          expire_clean_count++;
-        }else{
-          ite++;
+  info("start cleaning: cleaning interval = 2s, info log interval = 10s");
+  //TODO::这里先清理reqids，后面添加了 key 超时以后再设置 key 清理的逻辑
+  int count = 0;
+  int expire_clean_count = 0;
+  do{
+    count++;
+    usleep(clean_interval_);
+    {
+      std::unique_lock<std::shared_mutex> w_lock_for_ids(rw_lock_for_ids_);
+        auto cur_miliseconds = get_miliseconds();
+        //2s内的请求应该不多，直接遍历就好  
+        //TODO::这里看后面吧，要是影响比较大，就不写了。不多个锤子很多的
+        for(auto ite = req_ids_.begin();ite!= req_ids_.end();){
+          if(ite->second < cur_miliseconds){
+            ite = req_ids_.erase(ite);
+            expire_clean_count++;
+          }else{
+            ite++;
+          }
         }
       }
-    }
-    if(count==5){
-      info("clean log: clean {} expire req_ids_", expire_clean_count++);
-      count=0;
-      expire_clean_count=0;
-    }
-  }while(!exit_clean_flag_);
+      uint64_t curr_ts = get_miliseconds();
+      {
+        std::unique_lock<std::shared_mutex> lock(rw_lock_for_cache_);
+        while (!expire_queue_.empty()) {
+            const timed_key& tk = expire_queue_.top();
+            //debug("tk {} second {} {} {} {}", tk.first, tk.second->first, tk.second->second.first, tk.second->second.second, curr_ts);
+            if (tk.first < curr_ts) {
+                auto it = tk.second;
+                if (it != inner_cache_.end() && it->second.first < curr_ts) {
+                    info("clean {} value {} timestamp {} cur {} queue.size{}", it->first, it->second.second, it->second.first, curr_ts, expire_queue_.size());
+                    inner_cache_.erase(it);
+                }
+                expire_queue_.pop();
+            } else {
+                break;
+            }
+        }
+      }
+      //TEST
+//      while (!expire_queue_.empty()) {
+//          const timed_key& tk = expire_queue_.top();
+//          auto it = tk.second;
+//          info("clean {} value {} timestamp {} cur {} queue.size{} inner_cache_.size {}", it->first, it->second.second, it->second.first, curr_ts, expire_queue_.size(), inner_cache_.size());
+//          inner_cache_.erase(it);
+//          expire_queue_.pop();
+//      }
+
+
+      if(count==5){
+        info("clean log: clean {} expire req_ids_", expire_clean_count++);
+        count=0;
+        expire_clean_count=0;
+      }
+    }while(!exit_clean_flag_);
 }
 
 void KV_store_impl::run(){
@@ -170,6 +215,7 @@ void KV_store_impl::run(){
 }
 
 KV_store_impl::~KV_store_impl(){
+  exit_clean_flag_ = false;
   CH_node_serving_thr_->join();
   backend_update_thr_->join();
 }
--- ConsistentHashNode/KV_store.h
+++ ConsistentHashNode/KV_store.h
@@ -4,7 +4,10 @@
 #include <mutex>
 #include <thread>
 #include <unordered_map>
+#include <algorithm>
+#include <functional>
 #include <shared_mutex>
+#include <queue>
 
 #include "../pb/KV_store.grpc.pb.h"
 #include "CH_node_impl.h"
@@ -52,12 +55,23 @@ private:
     // 当然删除的时候是要做标记的，免得有请求过来的时候，写了相同的key，清理线程无感直接把这个给删除了，这样就不一致了
     std::shared_mutex rw_lock_for_ids_;
     std::unordered_map<uint32_t, uint64_t> req_ids_;
+
     //后面可以尝试 junction 仓库
+    using timed_key = std::pair<uint64_t, std::unordered_map<std::string, std::pair<uint64_t, std::string>>::iterator >;
+    using cache_type = std::unordered_map<std::string, std::pair<uint64_t, std::string>>;
+
     std::shared_mutex rw_lock_for_cache_;
-    std::unordered_map<std::string, std::string> inner_cache_;
+    std::unordered_map<std::string, std::pair<uint64_t, std::string>> inner_cache_;
+
+    struct timed_key_order {
+        bool operator()(const timed_key& lhs, const timed_key& rhs) const { return lhs.first > rhs.first; }
+    };
+
+    std::priority_queue<timed_key, std::vector<timed_key>, timed_key_order> expire_queue_;
+
     //后台更新清理线程:两个任务，清理 req_ids_,清理 inner_cache_
-    bool exit_clean_flag_;
-    uint clean_interval_= 2000000;
+    bool exit_clean_flag_ = false;
+    uint clean_interval_= 200000;
     std::shared_ptr<std::thread> backend_update_thr_;
     //CH_node
     std::shared_ptr<CH_node_impl> ch_node_;
